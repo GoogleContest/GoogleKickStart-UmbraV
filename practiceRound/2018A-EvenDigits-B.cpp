76112e04fcf38252d2b9be5287499e521588ff4a
Umbra Venus <shirleyrsj@outlook.com>
2020-02-13 08:56:23 -0600

    not correct yet

---
+++ practiceRound/2018A-EvenDigits-B.cpp
@@ -0,0 +1,119 @@
+// attempt after 50 submissions and white board flowchart
+
+# include <iostream>
+# include <cmath>
+# include <cstdlib>
+using namespace std;
+
+int plusY(int);
+int minusY(int);
+int degree(int);
+int first(int, int);
+
+int main()
+{
+    // Variables
+    int T;
+    int N;
+    int e, a, x;
+    int y;
+    int Plus, Minus;
+
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> N;
+        e = degree(N);
+        a = first(N, e);
+
+        if (e == 0)
+        {
+            y = 1;
+        }
+        else
+        {
+            Plus = abs(plusY(N));
+            Minus = abs(minusY(N));
+            y = min(Plus, Minus);
+        }
+        cout << "Case:#" << i << ": " << y << endl;
+    }
+    return 0;
+}
+
+int plusY(int n)
+{
+    int y;
+    int e = degree(n);
+    int b = n;
+
+    while (e >= 1)
+    {
+        int e = degree(b);
+        int a = first(b, e);
+        if (a == 9)
+        {
+            y = minusY(b);
+            return y;
+        }
+        else if (a % 2 == 0)
+        {
+            b = b - a * pow(10, e);
+        }
+        else
+        {
+            y = (a + 1) * pow(10, e) - n;
+            return y;
+        }
+    }
+}
+
+int minusY(int n)
+{
+    int x = 0;
+    int y;
+    int e = degree(n);
+    int a;
+    int b = n;
+
+    while (e >= 1)
+    {
+        e = degree(b);
+        a = first(b, e);
+        if (a % 2 == 0)
+        {
+            x += a * pow(10, e);
+            b = b - a * pow(10, e);
+        }
+        else
+        {
+            x += (a - 1) * pow(10, e);
+            int j;
+            for (j = 0; e >= 1; e--)
+            {
+                j = 8 * pow(10, e - 1);
+                x += j;
+            }
+        }
+    }
+    y = n - x;
+    return y;
+}
+
+int degree(int n)
+{
+    for (int e = 16; e >= 1; e--)
+    {
+        if (n / pow(10, e) >= 1)
+            return e;
+    }
+}
+
+int first(int n, int e)
+{
+    int a;
+    a = n / pow(10, e);
+    a = floor(a);
+    return a;
+}
--- practiceRound/2018A-EvenDigits-S.cpp
+++ practiceRound/2018A-EvenDigits-S.cpp
@@ -3,68 +3,42 @@
 // each char in number has to be even
 // 1 <= n <= 10^16
 
-#include <iostream> // includes cin to read from stdin and cout to write to stdout
+#include <iostream>
 #include <cmath>
-#include <cstdlib>   // absolute value
-using namespace std; // since cin and cout are both in namespace std, this saves some text
+#include <cstdlib> // absolute value
+using namespace std;
 
 int main()
 {
     /* Get info from input file */
-    int t;    // numbers of cases
-    int i;    // cased number
-    int e;    // pow(10, e), to determine how many digits are there
-    int y;    //output
-    cin >> t; // read t. cin knows that t is an int
+    int t;      // total numbers of test cases
+    int i;      // case No.
+    unsigned n; // the actual integar displayed
+    int e;      // pow(10, e), to determine how many digits are there, e <= 16
+    int y;      // output, how many button presses
+    int digit;  // first most digit
+    cin >> t;   // read t. cin knows that t is an int
+    int plus, minus;
 
-    for (i = 1; i <= t; i++)
+    for (i = 1; i <= t; i++) // iterate thru each integar
     {
-    }
-
-    cout << "Case #" << i << ": " << y << endl;
-
-    return 0;
-}
-
-/*for (int i = 1; i <= t; i++) // ??? Get other numbers?
-{
-    cin >> n;
-    if (n < 10) // single digit
-    {
-        y = 1;
-    }
-    else if (n == 100)
-    {
-        y = 12;
-    }
-    else
-    {
-        int first;
-        first = (int)floor(n / 10);
-        if (first % 2 == 0) // first digit is even
+        cin >> n;                     // get number
+        for (int e = 16; e >= 0; e--) // LOOP of each digit
         {
-            if (n % 2 == 0) // second digit is even
-            {
-                y = 0;
-            }
-            else //second digit is not even
-            {
-                y = 1;
-            }
-        }
-        else // first digit is not even
-        {
-            int second;              //let int a be the second digit
-            second = n - first * 10; //a = 3, y = 5, a = 8, y = 2, a = 6, y = 4; a = 4, y = 6; a = 2, y = 4
-            // when second < 4, y = second + 2, else y = 10 - second
-            if (second < 4)
-            {
-                y = second + 2;
-            }
+            if (digit < 1)
+                continue;
             else
             {
-                y = 10 - second;
+                if (digit % 2 != 0) // make each digit even,
+                {
+                    plus = ((digit + 1) * pow(10, e) - n);
+                    minus = (n - digit * pow(10, e) + 2);
+                    y = min(plus, minus);
+                    break;
+                }
             }
         }
+        cout << "Case:#" << i << ": " << y << endl;
     }
-    */
+    return 0;
+}
