d0d099e8be28f4e1ba2a94741c601491a6497a9b
Umbra V <60438078+UmbraVenus@users.noreply.github.com>
2020-08-02 10:36:31 -0700

    H_index small dataset done

--- ProblemA/H_Index.py
+++ ProblemA/H_Index.py
@@ -2,12 +2,15 @@ T = int(input())
 for t in range(1, T + 1):
     N = int(input())
     s = [int(x) for x in input().split()]
-    h = [1]
+    h = "1"
     x = 1
     for i in range(1, N):
         lis = s[: i + 1]
         lis.sort(reverse=True)
-        if lis[i] >= i:
+        # print("i = {} while Lis = {}".format(i, lis))
+        if lis[x] > x:
             x += 1
-        h.append(x)
+            # print("lis[x] = {}, x = {}".format(lis[i], i))
+        h += ' ' + str(x)
+        # print("h = {}".format(h))
     print("Case #{}: {}".format(t, h))
